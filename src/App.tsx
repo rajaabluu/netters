439cb6a5e7512febd24364cea162ea99072036ca
Raajaazz <aunurja@gmail.com>
2024-07-18 14:40:02 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
